796c98fc2d5e3b7654a75f9a70c887703de261ea
guoq <guoq@tingyun.com>
2019-01-23 14:41:50 +0800

    first commit

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * TestController
+ *
+ * @author WuYuxiang
+ * @date 2018/9/20
+ */
+@RestController
+@RequestMapping("/")
+public class TestController {
+    @RequestMapping("springboot")
+    public String testSpringBoot() {
+        return "Hello Spring Boot ! ";
+    }
+}
